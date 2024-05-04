56bb87cb8ba323596a863eb227a922d0993e79f3
Huaranga-Huaman-Luis-Enrique <huarangaluis86@gmail.com>
2024-05-04 10:51:30 -0500

    FINALIZADO

---
+++ app/src/main/java/com/example/semana06/service/ServiceLibro.java
@@ -0,0 +1,18 @@
+package com.example.semana06.service;
+
+
+import com.example.semana06.entity.Categoria;
+import com.example.semana06.entity.Libro;
+import com.example.semana06.entity.Pais;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface ServiceLibro {
+    @POST("servicio/libro")
+    public Call<Libro> Registra(@Body Libro objLibro);
+}
---
+++ app/src/main/java/com/example/semana06/service/ServicePais.java
@@ -0,0 +1,15 @@
+package com.example.semana06.service;
+
+import com.example.semana06.entity.Pais;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ServicePais {
+
+    @GET("servicio/util/listaPais")
+    public Call<List<Pais>> listaTodos();
+}
